936b8550acc2654a084afb6ac519c0299521a8d2
salihacr <salihacur1@gmail.com>
2021-03-18 22:09:10 +0300

    repository and uow added

---
+++ E-Commerce-App.Data/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using E_Commerce_App.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace E_Commerce_App.Data
+{
+    public class AppDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+
+        }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<CartItem> CartItems { get; set; }
+        public DbSet<Cart> Carts { get; set; }
+        public DbSet<OrderItem> OrderItems { get; set; }
+        public DbSet<Order> Orders { get; set; }
+    }
+}
---
+++ E-Commerce-App.Data/UnitOfWorks/UnitOfWork.cs
@@ -0,0 +1,28 @@
+﻿using E_Commerce_App.Core.UnitOfWorks;
+using System.Threading.Tasks;
+
+namespace E_Commerce_App.Data.UnitOfWorks
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly AppDbContext _context;
+        public UnitOfWork(AppDbContext context)
+        {
+            _context = context;
+        }
+        public void Commit()
+        {
+            _context.SaveChanges();
+        }
+
+        public async Task CommitAsync()
+        {
+            await _context.SaveChangesAsync();
+        }
+
+        public void Dispose()
+        {
+            _context.Dispose();
+        }
+    }
+}
