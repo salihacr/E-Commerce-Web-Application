3cc82025340a9a1740c1873261d909c9a55ceeb3
salihacr <salihacur1@gmail.com>
2021-04-01 19:33:37 +0300

    product detail page is editing

---
+++ E-Commerce-App.WebUI/Controllers/CartController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace E_Commerce_App.WebUI.Controllers
+{
+    public class CartController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
