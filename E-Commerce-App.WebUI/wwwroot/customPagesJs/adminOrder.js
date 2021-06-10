f2d331062a3c5f7155bc3377e663d0e6669ff0d6
salihacr <salihacur1@gmail.com>
2021-06-10 22:14:09 +0300

    admin order css and js files edited

---
+++ E-Commerce-App.WebUI/wwwroot/customPagesJs/adminOrder.js
@@ -0,0 +1,32 @@
+﻿$(document).ready(function () {
+    $('#form-order-state').on('submit', function (event) {
+        var formData = new FormData($('#form-order-state')[0]);
+        $.ajax({
+            type: 'POST',
+            url: "/Admin/Order/EditOrderState",
+            data: formData,
+            contentType: false,
+            processData: false,
+            success: async function (res) {
+                if (res.success) {
+                    await Swal.fire(
+                        'Kayıt Başarılı !',
+                        `${res.message}`,
+                        'success'
+                    )
+                    window.location.href = res.redirectUrl;
+                } else {
+                    Swal.fire(
+                        'Kayıt Hatası !',
+                        `${res.message}`,
+                        'error'
+                    )
+                }
+            },
+            error: function (err) {
+                toastr.error("Beklenmedik bir hata meydana geldi");
+            }
+        });
+        return false;
+    });
+});
---
+++ E-Commerce-App.WebUI/wwwroot/customPagesJs/adminOrderList.js
@@ -0,0 +1,6 @@
+﻿$(document).ready(function () {
+    $("#orders-table").DataTable({
+        "responsive": true, "lengthChange": false, "autoWidth": false,
+        "buttons": ["excel", "csv"]
+    }).buttons().container().appendTo('#orders-table_wrapper .col-md-6:eq(0)');
+});
